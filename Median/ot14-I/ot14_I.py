715a659ff5fef5ff4f94c05a407ab48241456c6b
Collapseyu <719144252@qq.com>
2021-03-03 14:43:24 +0800

    update ot14-I

--- Median/ot14-I/ot14_I.py
+++ Median/ot14-I/ot14_I.py
@@ -1,4 +1,5 @@
 from typing import List
+import math
 class Solution:
     def cuttingRope(self, n: int) -> int:
         if n < 4:
@@ -10,3 +11,15 @@ class Solution:
             dp[i] = max(dp[i],i)
         # print(dp)
         return dp[-1]
+    def cuttingRope_greedy(self,n:int) -> int:
+        if n<=3:
+            return n-1
+        tmp_n = (n//3) - 1
+        left_num = (n%3)
+        res = int(math.pow(3,tmp_n))
+        if 3 - left_num > left_num:
+            res *= (3+left_num)
+        else:
+            res *= (left_num*3)
+        return res
+        
